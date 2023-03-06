518ccf2b3ad9e6c8c917dc8dbef94674a3ef4ee2
kaique winicios <kaiquew@outlook.com>
2023-03-06 20:05:57 -0300

    dialect

--- src/models/database/db.ts
+++ src/models/database/db.ts
@@ -5,7 +5,10 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export const db = new Sequelize(
-    process.env.URL as string
+    process.env.URL as string,
+    {
+        dialect: 'postgres' 
+    }
 );
 
 export default db;
